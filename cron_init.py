15480b0735fbc07543039f84f00a43d9a8dfb34d
Gregory Smith <gpsmith@princeton.edu>
2020-09-08 12:32:50 -0400

    removed unnecessary files for demo

--- cron_init.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from crontab import CronTab
-
-cron = CronTab(user=True)
-job = cron.new(command="$HOME/alariss-scheduler/bin/python $HOME/scheduler_app/send_reminders.py")
-job.every().dom()
-
-cron.write()
